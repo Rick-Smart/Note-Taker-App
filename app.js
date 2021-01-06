04b7256649b392a72e9f11d94356339644a3d320
Xis <rick-s@hotmail.com>
2021-01-05 23:44:33 -0800

    init

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 3000 || process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
